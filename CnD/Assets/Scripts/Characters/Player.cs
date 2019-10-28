6e319bbe0bc3a3459b7cbee7fde674391ba960f7
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-10-28 12:50:38 -0400

    implemented movement
    basic movement controls have been implemented

---
+++ CnD/Assets/Scripts/Characters/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public float movSpeed;
+
+    private Transform pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos.Translate(Input.GetAxis("Horizontal") * movSpeed, Input.GetAxis("Vertical") * movSpeed, 0f);
+    }
+}
