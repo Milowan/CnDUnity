2500c4f3b598b124b758907b3d5ab52289bbe511
StevenCoverdale <S.coverdale@hotmail.com>
2019-10-28 16:45:59 -0400

    Project

---
+++ CnD/Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum CharacterStatus
+    {
+        IDLE,
+        FIGHTING,
+        CHASING,
+        MOVING,
+        DEAD
+    };
+
+    public CharacterStatus status;
+    public float maxHealth;
+    private float health;
+    public float movSpeed;
+
+    //private Stats stats;
+    protected float combatTimer;
+    protected Character target;
+
+    protected virtual void Attack()
+    {
+
+    }
+
+    protected void TakeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+
+    }
+}
