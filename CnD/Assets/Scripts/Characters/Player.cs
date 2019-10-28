0ce681ed6547011385cc2fbe82cf2fd191c62300
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-10-28 14:48:34 -0400

    fleshed out character classes
    added various attributes to character classes such that their base functionality can be implemented

--- CnD/Assets/Scripts/Characters/Player.cs
+++ CnD/Assets/Scripts/Characters/Player.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class Player : Character
 {
-    public float movSpeed;
-
     private Transform pos;
+
+    //private Sword *sword;
+    //private Armour *armour;
+    //private Helmet *helmet;
+    //private Inventory *inventory;
+    //private Interactable *interactable;
 
     // Start is called before the first frame update
     void Start()
     {
         pos = GetComponent<Transform>();
+        pos.tag = "Player";
     }
 
     // Update is called once per frame
     void Update()
     {
         pos.Translate(Input.GetAxis("Horizontal") * movSpeed, Input.GetAxis("Vertical") * movSpeed, 0f);
+    }
+
+    private void OnTriggerEnter(Collider response)
+    {
+
     }
 }
