1757a79124dc73586c050a77b7e65756808eb351
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-10-29 14:47:36 -0400

    added class fundamentals for enemies

---
+++ CnD/Assets/Scripts/Stats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats
+{
+    public float attack;
+    public float defense;
+    public float evasion;
+
+    Stats()
+    {
+        attack = 0;
+        defense = 0;
+        evasion = 0;
+    }
+}
