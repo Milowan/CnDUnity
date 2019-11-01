6f9882ea11ea9a26bcb7cefd63223e18d4edc1a3
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-01 17:02:08 -0400

    Started to create the Start Menu
    -Began to create the Start Menu Scene with buttons for Start and Exit application

---
+++ CnD/Assets/Scripts/Scenes/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance = null;
+    public GameObject startMenu;
+    public GameObject floor1;
+    public GameObject floor2;
+    void Awake()
+    {
+        startMenu.SetActive(true);
+        SetInstance();
+    }
+
+    void SetInstance()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ CnD/Assets/Scripts/Scenes/StartMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        Scene scene;
+        scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
