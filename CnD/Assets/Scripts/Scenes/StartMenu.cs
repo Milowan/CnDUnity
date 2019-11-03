cc346faa6b03564ea0a9c80dfa5128be6c46c089
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-03 16:04:25 -0500

    Settings Menu
    -Added FullScreen Option
    -Added Quality DropDown
    -Added Resolution DropDown

--- CnD/Assets/Scripts/Scenes/StartMenu.cs
+++ CnD/Assets/Scripts/Scenes/StartMenu.cs
@@ -2,8 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class StartMenu : MonoBehaviour
 {
+    public Dropdown screenResolutionDD;
+    Resolution[] resolutionList;
+
+    public void Start()
+    {
+        resolutionList = Screen.resolutions;
+
+        screenResolutionDD.ClearOptions();
+
+        List<string> resolutionOptions = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutionList.Length; i++)
+        {
+            string option = resolutionList[i].width + "x" + resolutionList[i].height;
+            resolutionOptions.Add(option);
+
+            if (resolutionList[i].width == Screen.currentResolution.width && resolutionList[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        screenResolutionDD.AddOptions(resolutionOptions);
+        screenResolutionDD.value = currentResolutionIndex;
+        screenResolutionDD.RefreshShownValue();
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutionList[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void setScreenQuality(int screenQualityIndex)
+    {
+        QualitySettings.SetQualityLevel(screenQualityIndex);
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -20,4 +63,6 @@ public class StartMenu : MonoBehaviour
         scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
     }
+
+
 }
