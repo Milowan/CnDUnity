98895a6180b98d78cd2ef2cf1d7d436573ba81d4
eitrunix <52928831+eitrunix@users.noreply.github.com>
2019-11-02 23:47:09 -0400

    Removing Inventory PT1
    Removing hte Current inventory system as its not working the way we want, adding a new one that will.

---
+++ CnD/Assets/Scripts/Items/ItemScripts/DropSCript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropSCript : MonoBehaviour
+{
+    public GameObject item;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void SpawnDroppedItem()
+    {
+        Vector3 playerPos = new Vector3(player.position.x, player.position.y + 1, player.position.z);
+        Instantiate(item, playerPos, Quaternion.identity);
+    }
+}
